416da0f74a73e21c91ed507e516f708f68902889
Rannielyn Raneses <rgraneses7@gmail.com>
2018-10-19 18:20:52 +0800

    Created functions
    Signed-off-by: Rannielyn Raneses <rgraneses7@gmail.com>

--- index.js
+++ index.js
@@ -24,3 +24,58 @@ conn.connect(errors => {
 app.listen(8080, () => {
 	console.log('Express server started on port 8080');
 });
+
+app.get('/', (request,response) =>{
+	conn.query('SELECT * FROM ITEMS', (errors, rows, fields) => {
+		response.render('index',{
+			title: 'Item Table',
+			items: rows
+		})
+	})
+})
+
+
+app.get('/items', (request, response) => {
+	conn.query('SELECT * FROM ITEMS', (errors, rows, fields) => {
+		response.send(errors ? errors : rows);
+	})
+})
+
+app.get('/editItem/:id', (request, response) => {
+	conn.query('SELECT * FROM ITEMS WHERE ID = ?', [request.params.id] ,(errors, rows, fields) => {
+		response.render('editItem',{
+			title: 'Edit Item',
+			items: rows[0]
+		})
+	})
+})
+
+app.get('/deleteItem/:id', (request, response) => {
+	conn.query('DELETE FROM ITEMS WHERE ID = ?', [request.params.id] ,(errors, rows, fields) => {
+		//response.send(errors ? errors : 'Successfully deleted');
+		response.redirect('/');
+	})
+})
+
+app.post('/updateItem/:id', (request, response) => {
+	let data = request.body;
+	conn.query('UPDATE ITEMS SET name=?, qty=?, amount=? WHERE ID=?', [data.name, data.qty, data.amount, request.params.id] ,(error, rows, fields) => {
+		//response.send(error ? error : 'Updated Successfully');
+		response.redirect('/');
+	})
+})
+
+app.post('/addItem', (request, response) => {
+	let data = request.body;
+	conn.query('INSERT INTO ITEMS (NAME, QTY, AMOUNT) VALUES(?, ?, ?)', [data.name, data.qty, data.amount] ,(error, rows, fields) => {
+		//response.send(error ? error : 'Updated Successfully');
+		response.redirect('/');
+	})
+})
+
+app.get('/addItem',(request,response) => {
+	response.render('itemForm', {
+		title : 'Add Item'
+	})
+})
+
